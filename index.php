523be66fb08e4baa9aab0af899776a9c12997a30
Juan Kuga <juankp3@gmail.com>
2024-03-10 21:52:27 -0500

    base

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+// If you're using Composer, require the autoloader.
+require 'vendor/autoload.php';
+// if you're not using Composer, load the framework directly
+// require 'flight/Flight.php';
+
+// Then define a route and assign a function to handle the request.
+Flight::route('/', function () {
+  echo 'hello world!';
+});
+
+// Finally, start the framework.
+Flight::start();
---
+++ vendor/composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'name' => 'juankuga/php8',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'flightphp/core' => array(
+            'pretty_version' => 'v3.6.0',
+            'version' => '3.6.0.0',
+            'reference' => '735dafefb9b49ef46f26a511efe224d575c28cc7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../flightphp/core',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'juankuga/php8' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mikecao/flight' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.0.2',
+            ),
+        ),
+    ),
+);
